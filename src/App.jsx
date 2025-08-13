8f0299199264cb3f528a0e7d543648df36b148aa
morgantstewart <morgan@paysigil.com>
2025-08-12 23:40:12 -0400

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import '/src/components/NavBar/NavBar.jsx';
 import '/src/components/MailboxForm/MailboxForm.jsx';
 import '/src/components/MailboxList/MailboxList.jsx';
 import '/src/components/MailboxDetails/MailboxDetails.jsx';
+import { Route, Routes } from 'react-router';
 
 const App = () => {
   return <h1>Hello world!</h1>;
 };
+
+
+      <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+       <Route path="/mailboxes" element={<MailboxList />} />
+         <Route path="/new-mailbox" element={<MailboxForm />} />
+          <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
+      </Routes>
+
 
 export default App;
