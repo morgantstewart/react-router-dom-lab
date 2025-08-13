bc41d778b3ef8e6a477d229dd5a100d770832ecd
morgantstewart <morgan@paysigil.com>
2025-08-12 22:06:20 -0400

    created and imported components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-// src/App.jsx
+import from './src/components/NavBar/NavBar.jsx';
+import from './src/components/MailboxForm/MailboxForm.jsx';
+import from '/src/components/MailboxList/MailboxList.jsx';
+import from './src/components/MailboxDetails/MailboxDetails.jsx';
 
 const App = () => {
   return <h1>Hello world!</h1>;
